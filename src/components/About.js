297dce8806547937acb0e73c24c7af729ffa712f
dalienst <oduordalienst@gmail.com>
2022-05-19 17:39:57 +0300

    pages

--- src/components/About.js
+++ src/components/About.js
@@ -11,18 +11,20 @@ export default function About() {
                             I love to build applications and podcasting
                     </h1>
                     <p className="mb-8 leading-relaxed">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                    laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                    Laborum, voluptas natus?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+                        laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+                        Laborum, voluptas natus?
                     </p>
                     <div className="flex justify-center">
                         <a href="#contact" className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Work with me</a>
-                        <a href="#projects" className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                            See my previous works
-                        </a>
+                        <a href="#projects" className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">See my previous works</a>
                     </div>
+                </div>
+
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                    <img className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 rounded-md" alt="hero" src="./me.jpeg"/>
                 </div>
             </div>
         </section>
-    )
+    );
 }
---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const projects = [
+    {
+        title: "Hospital Management System",
+        subtitle: "HTML, CSS, PHP",
+        description: "A hospital record system",
+        image: "./hms.jpeg",
+        link: "#/hms",
+    },
+    {
+        title: "Hospital Management System",
+        subtitle: "HTML, CSS, PHP",
+        description: "A hospital record system",
+        image: "./hms.jpeg",
+        link: "#/hms",
+    },
+    {
+        title: "Hospital Management System",
+        subtitle: "HTML, CSS, PHP",
+        description: "A hospital record system",
+        image: "./hms.jpeg",
+        link: "#/hms",
+    },
+    {
+        title: "Hospital Management System",
+        subtitle: "HTML, CSS, PHP",
+        description: "A hospital record system",
+        image: "./hms.jpeg",
+        link: "#/hms",
+    },
+]
+export const  skills = ['Flutter', 'Python', 'ReactJS', 'Java', 'Web Development']
